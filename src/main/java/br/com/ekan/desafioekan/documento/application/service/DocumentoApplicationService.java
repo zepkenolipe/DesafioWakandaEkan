a9b0c9925fc5f8c9c54512d250fe64362557c55d
phellipe <phelliperrp@gmail.com>
2025-08-15 00:11:01 -0300

    implementa api, controller e service

--- src/main/java/br/com/ekan/desafioekan/documento/application/service/DocumentoApplicationService.java
+++ src/main/java/br/com/ekan/desafioekan/documento/application/service/DocumentoApplicationService.java
@@ -1,15 +1,17 @@
 package br.com.ekan.desafioekan.documento.application.service;
 
 import br.com.ekan.desafioekan.beneficiario.application.service.BeneficiarioService;
+import br.com.ekan.desafioekan.documento.application.api.DocumentoBeneficiarioListResponse;
 import br.com.ekan.desafioekan.documento.application.api.DocumentoRequest;
 import br.com.ekan.desafioekan.documento.application.api.DocumentoResponse;
-import br.com.ekan.desafioekan.documento.domain.Documento;
 import br.com.ekan.desafioekan.documento.application.repository.DocumentoRepository;
+import br.com.ekan.desafioekan.documento.domain.Documento;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,5 +28,14 @@ public class DocumentoApplicationService implements DocumentoService {
         Documento documento = documentoRepository.salvaDocumento(new Documento(idBeneficiario, documentoRequest));
         log.info("[finaliza] DocumentoApplicationService - criaDocumento");
         return new DocumentoResponse(documento.getIdDocumento());
+    }
+
+    @Override
+    public List<DocumentoBeneficiarioListResponse> buscaDocumentosDoBeneficiarioComID(UUID idBeneficiario) {
+        log.info("[inicia] DocumentoApplicationService - buscaDocumentosDoBeneficiarioComID");
+        beneficiarioService.buscaBeneficiarioAtravesId(idBeneficiario);
+        List<Documento> documentosDoCliente = documentoRepository.buscaDocumentosDoBeneficiarioComID(idBeneficiario);
+        log.info("[finaliza] DocumentoApplicationService - buscaDocumentosDoBeneficiarioComID");
+        return DocumentoBeneficiarioListResponse.converte(documentosDoCliente);
     }
 }
--- src/main/java/br/com/ekan/desafioekan/documento/application/service/DocumentoService.java
+++ src/main/java/br/com/ekan/desafioekan/documento/application/service/DocumentoService.java
@@ -1,11 +1,15 @@
 package br.com.ekan.desafioekan.documento.application.service;
 
+import br.com.ekan.desafioekan.documento.application.api.DocumentoBeneficiarioListResponse;
 import br.com.ekan.desafioekan.documento.application.api.DocumentoRequest;
 import br.com.ekan.desafioekan.documento.application.api.DocumentoResponse;
 import jakarta.validation.Valid;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DocumentoService {
     DocumentoResponse criaDocumento(UUID idBeneficiario, @Valid DocumentoRequest documentoRequest);
+
+    List<DocumentoBeneficiarioListResponse> buscaDocumentosDoBeneficiarioComID(UUID idBeneficiario);
 }
